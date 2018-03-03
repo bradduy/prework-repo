3283cbaaaaa6f9797f749d893ce77c7966d87528
Brad Duy <33892919+bradduy@users.noreply.github.com>
2018-03-03 10:29:05 +0700

    Add files via upload

---
+++ blockchain.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+// Blockchain is our global blockchain.
+var Blockchain []Block
+
+// Block is our basic data structure!
+type Block struct {
+	Data      string
+	Timestamp int64
+	PrevHash  []byte
+	Hash      []byte
+}
+
+// InitBlockchain creates our first Genesis node.
+func InitBlockchain() {
+	genesisBlock := Block{"Genesis Block", time.Now().Unix(), []byte{}, []byte{}}
+	genesisBlock.Hash = genesisBlock.calculateHash()
+	Blockchain = []Block{genesisBlock}
+}
+
+// NewBlock creates a new Blockchain Block.
+func NewBlock(oldBlock Block, data string) Block {
+	block := Block{data, time.Now().Unix(), []byte{}, []byte{}}
+	block.PrevHash = oldBlock.Hash
+	block.Hash = block.calculateHash()
+	return block
+}
+
+// AddBlock adds a new block to the Blockchain.
+func AddBlock(b Block) error {
+	fmt.Println("******TODO: IMPLEMENT AddBlock!******")
+	spew.Dump(Blockchain)
+	// Fill me in, brave wizard.
+	return nil
+}
+
+func (b *Block) calculateHash() []byte {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	data := []byte(b.Data)
+	headers := bytes.Join([][]byte{b.PrevHash, data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	return hash[:]
+}
